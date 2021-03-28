b50747f755c29942fa41fd4641544023f5066442
LiKunlong <lkl0801@sina.cn>
2021-03-28 08:56:41 +0800

    提交

---
+++ C#经典代码/1-窗体技巧/自定义窗体的最大化、最小化和关闭按钮/自定义窗体的最大化、最小化和关闭按钮/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace 自定义窗体的最大化_最小化和关闭按钮
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
