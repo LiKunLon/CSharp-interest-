f3e0c98ccc65929adbb2deb396e60e8ad0537402
LiKunlong <lkl0801@sina.cn>
2021-03-28 14:04:33 +0800

    提交

---
+++ C#经典代码/1-窗体技巧/获取文件目录/FileCatalog/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileCatalog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;							//设置拖放操作中目标放置类型为复制
+            String[] str_Drop = (String[])e.Data.GetData(DataFormats.FileDrop, true);//检索数据格式相关联的数据
+            MessageBox.Show(str_Drop[0]);
+        }
+    }
+}
