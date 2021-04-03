3bd731d92809f5b7dffe4ef327a861bdb175c9b5
LiKunlong <lkl0801@sina.cn>
2021-04-03 18:02:37 +0800

    提交

---
+++ C#经典代码/5-系统操作/设置和屏蔽系统热键/热键/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace 设置和屏蔽系统热键
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
