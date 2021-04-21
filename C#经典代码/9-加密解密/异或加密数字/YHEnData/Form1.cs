c2cfd73d6f5dd0ac2c34315e884a32eee8e07b28
LiKunlong <lkl0801@sina.cn>
2021-04-21 13:49:31 +0800

    提交

---
+++ C#经典代码/9-加密解密/异或加密数字/YHEnData/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YHEnData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    textBox2.Text = (Convert.ToInt32(textBox1.Text) ^ 123).ToString();
+                }
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox2.Text != "")
+                {
+                    textBox1.Text = (Convert.ToInt32(textBox2.Text) ^ 123).ToString();
+                }
+            }
+            catch { }
+        }
+    }
+}
