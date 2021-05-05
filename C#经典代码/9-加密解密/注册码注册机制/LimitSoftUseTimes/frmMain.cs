f673064d0360708e97cfbd43351cbb070f6a8f1e
LiKunlong <lkl0801@sina.cn>
2021-05-05 10:17:01 +0800

    提交

---
+++ C#经典代码/9-加密解密/注册码注册机制/LimitSoftUseTimes/frmMain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using ClassLibrary1;
+
+namespace LimitSoftUseTimes
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        SoftReg softreg = new SoftReg();//实例化公共类对象
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            RegistryKey retkey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("software", true).CreateSubKey("mrwxk").CreateSubKey("mrwxk.ini");
+            foreach (string strRNum in retkey.GetSubKeyNames())//判断是否注册
+            {
+                if (strRNum == softreg.getRNum())
+                {
+                    this.Text = "主窗体（已注册）";
+                    button1.Enabled = false;
+                    return;
+                }
+            }
+            this.Text = "主窗体（未注册）";
+            button1.Enabled = true;
+            MessageBox.Show("您现在使用的是试用版，该软件可以免费试用30次！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Int32 tLong;
+            try
+            {
+                tLong = (Int32)Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\tryTimes", "UseTimes", 0);
+                MessageBox.Show("感谢您已使用了" + tLong + "次", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\tryTimes", "UseTimes", 0, RegistryValueKind.DWord);
+                MessageBox.Show("欢迎新用户使用本软件", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            tLong = (Int32)Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\tryTimes", "UseTimes", 0);
+            if (tLong < 30)
+            {
+                int Times = tLong + 1;
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\tryTimes", "UseTimes", Times);
+            }
+            else
+            {
+                MessageBox.Show("试用次数已到", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Exit();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmRegister frmregister = new frmRegister();
+            frmregister.Show();
+            this.Hide();
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
